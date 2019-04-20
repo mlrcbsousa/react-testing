4a64d786f96abae090371c4559ebfed729922aab
Manuel Sousa <mlrcbsousa@gmail.com>
2019-04-20 21:34:01 +0200

    first commit testing

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../assets/stylesheets/application.scss';
+import Hello from './components/hello';
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(<Hello name="World" />, root);
+}
